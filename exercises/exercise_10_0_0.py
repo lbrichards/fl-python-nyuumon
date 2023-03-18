fb27d075f64eede5792c9fcc98e1a2e4c942a1bb
Larry Richards <lrichards@iotatx.com>
2023-03-18 15:22:21 +0900

    new exercise

--- exercises/exercise_10_0_0.py
+++ exercises/exercise_10_0_0.py
@@ -1,19 +1,20 @@
 # define the add_numbers function
-def divide_numbers(a, b):
-    return a /b
+def add_numbers(a, b):
+    return # your answer here
 
 # ---
 def test_add_numbers():
     # test case 1
-    test_output = divide_numbers(0, 1)
+    test_output = add_numbers(0, 1)
     correct_answer = 1
     if test_output == correct_answer:
         print(f"正解です。出力：{test_output}")
     else:
         print(f"""
-残念でした。もう一度チャレンジしましょう。
-今回の要求出力は「{correct_answer}」でしたが、「{test_output}」が出力されました。
+        残念でした。もう一度チャレンジしましょう。
+        今回の要求出力は「{correct_answer}」でしたが、「{test_output}」が出力されました。
         """)
+
 
 # call the test function
 test_add_numbers()
