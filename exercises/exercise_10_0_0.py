4bde1539e6298080d721e2bba64b813b650e5165
Larry Richards <lrichards@iotatx.com>
2023-03-18 15:51:57 +0900

    new exercise

--- exercises/exercise_10_0_0.py
+++ exercises/exercise_10_0_0.py
@@ -1,12 +1,35 @@
-# define the add_numbers function
-def add_numbers(a, b):
-    return # your answer here
+"""
+datetimeモジュール、dateutil.parser.parse関数を使用して、
+2つの日付の間の日数を求めるプログラムを作成してください。二つの
+日数がマイナスにならないように心がけてください。つまり、低い日付と
+高い日付を入力する順番に依存しないようにする。
+
+
+プログラムは、ユーザーに2つの日付を入力するように促し、それらの日付の間の日数をコンソールに出力します。
+"""
+import datetime
+from dateutil.parser import parse
+
+def how_many_days_between(date1_str, date2_str):
+    # 文字列からdatetime.dateオブジェクトを作る
+    date1 = parse(date1_str).date()
+    date2 = parse(date2_str).date()
+    return abs((date2-date1).days)
+
+def main():
+    # Prompt the user for the two dates
+    date1_str = input("Enter the first date in YYYY-MM-DD format: ")
+    date2_str = input("Enter the second date in YYYY-MM-DD format: ")
+    ndays = how_many_days_between(date1_str, date2_str)
+    print(f"There are {ndays} days between {date1_str} and {date2_str}.")
+
 
 # ---
-def test_add_numbers():
-    # test case 1
-    test_output = add_numbers(0, 1)
-    correct_answer = 1
+def test():
+    s2 = '2025-03-11'
+    s1 = '2026-03-11'
+    test_output = how_many_days_between(s1, s2)
+    correct_answer = 365
     if test_output == correct_answer:
         print(f"正解です。出力：{test_output}")
     else:
@@ -17,4 +40,4 @@ def test_add_numbers():
 
 
 # call the test function
-test_add_numbers()
+test()
