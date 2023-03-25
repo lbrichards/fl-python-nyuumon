9371e7fba198b5212393315acc4909c212d70423
Larry Richards <lrichards@iotatx.com>
2023-03-25 09:45:18 +0900

    added Lesson 5 problem

---
+++ exercises/exercise_05_0_0.py
@@ -0,0 +1,42 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+# for loopをbreakで中断する練習問題   #
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+
+"""
+     ------   問題解説   -------
+
+振り子長さを入力として振り子の周波数を返す関数を書きましょう。
+
+長さ(m)をLと書いた場合、周波数fは次の式で求められます。fの単位はHzです。
+
+f = 1/(2π) √(g/L)
+
+gは重力加速度（m/s^2）の定数です
+"""
+
+from math import pi, sqrt
+g = 9.8
+def calc_frequency(L):
+    return 1/(2*pi) * sqrt(g * L)
+
+L = 1.5
+f = calc_frequency(L)
+print(f"A pendulum with length {L} meters has a frequency of {f:0.2f} Hz")
+
+
+# ---
+
+def test():
+    L = 1.5
+    epsilon = 1e-10
+    found = calc_frequency(L)
+    expected = 1/(2*pi) * sqrt(g * L)
+    if found - expected < epsilon:
+        print(f"正解です。出力：{found}")
+    else:
+        print(f"""
+残念でした。もう一度チャレンジしましょう。
+今回の要求出力は「{expected}」でしたが、「{found}」が出力されました。
+                """)
+
+test()
