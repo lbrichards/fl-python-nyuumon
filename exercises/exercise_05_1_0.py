ae691a45c6af4a96225d96809d668199ca8379de
Larry Richards <lrichards@iotatx.com>
2023-03-25 10:49:12 +0900

    adding new Lesson 5 problem

---
+++ exercises/exercise_05_1_0.py
@@ -0,0 +1,45 @@
+    # =-=-=-=-=-=-=-=　#
+    #  独自関数の練習   #
+    # =-=-=-=-=-=-=-=　#
+
+"""
+     ------   問題解説   -------
+数値のリストを入力とし、返り値を、各要素の２乗を
+足し算した合計とする。たとえば、
+[2,3]が入力の場合、返り値は
+2^2 + 3^3 => 13となる。関数名は
+sum_of_squaresとし、named argのnumbersを利用する。
+numbersのデフォルト値は空リスト[]とし、そのままの入力の
+場合は、返り値を0とする。
+"""
+
+def sum_of_squares(numbers = []):
+    result = 0
+    # ここに関数の振る舞いを書いてください。
+    return result
+
+# 関数をテスト
+print(sum_of_squares([])) # "0"
+print(sum_of_squares([2,2])) # "8"
+print(sum_of_squares([1,2,3,4,5])) # "55"
+
+# ---
+
+def test():
+    found = sum_of_squares([1,2,3,4,5])
+    expected = 55
+    if found == expected:
+        print(f"正解です。出力：{found}")
+    else:
+        print(f"""
+残念でした。もう一度チャレンジしましょう。
+今回の要求出力は「{expected}」でしたが、「{found}」が出力されました。
+                """)
+
+"""
+Correct answer:
+    for num in numbers:
+        result += num ** 2
+"""
+
+test()
