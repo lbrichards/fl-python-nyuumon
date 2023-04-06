0a3b48669211b35f592b168805bf44b2a5f82bc0
Larry Richards <lrichards@iotatx.com>
2023-04-06 12:44:52 +0900

    refactoring

---
+++ exercises/exercise_3_0_0.py
@@ -0,0 +1,33 @@
+"""
+左下にPython Consoleがあります。最初から「＞＞＞」と表示されます。
+「＞＞＞」とは、「次の入力をどうぞ」という意味です。ですから、自分で
+「＞＞＞」を入れる必要はありません。
+
+レッスンのビデオと同じようなコマンドを打つことでPythonの練習ができます。
+なんでも試してみましょう。
+例えば…
+
+
+
+>>> pi = 3.14
+>>> type(pi)
+>>> r = 3
+>>> pi * r ** 2
+>>> ninzu = 10
+>>> piza_goto_no_slice = 4
+>>> hitori_goto_no_slice = 2
+>>> slice_goukei = ninzu * hitori_goto_no_slice
+>>> slice_goukei
+>>> piza_goukei = slice_goukei / piza_goto_no_slice
+>>> piza_goukei
+"""
+
+
+
+# ---
+
+s = """
+このレッスンにプログラミング・テストはありません。
+"""
+
+print(s)
---
+++ exercises/exercise_3_1_0.py
@@ -0,0 +1,57 @@
+"""
+左下にPython Consoleがあります。最初から「＞＞＞」と表示されます。
+「＞＞＞」とは、「次の入力をどうぞ」という意味です。ですから、自分で
+「＞＞＞」を入れる必要はありません。
+
+レッスンのビデオと同じようなコマンドを打つことでPythonの練習ができます。
+なんでも試してみましょう。
+例えば…
+
+
+
+>>> mylist = ["a", 3.14, -6, 10]
+
+>>> mylist
+
+>>> mylist[2]
+
+>>> mylist[1:3]
+
+>>> mylist[1:]
+
+>>> mylist[-2:]
+
+>>> sentence = "mukashi mukashi sono mukashi"
+
+>>> sentence.split()
+
+>>> type(sentence.split())
+
+>>> words = sentence.split()
+
+>>> words
+>>> words[3:]
+>>> words[-2:]
+>>> words.sort()
+>>> words
+
+>>> dir([])
+
+>>> words.append(123)
+>>> words
+>>> dir(words)
+>>> words.count("mukashi")
+>>> words.count("sono")
+>>> words.count("SONO")
+
+"""
+
+
+
+# ---
+
+s = """
+このレッスンにプログラミング・テストはありません。
+"""
+
+print(s)
