433daee59a6308414efa048fa53f505ba20e0c95
Larry Richards <lrichards@iotatx.com>
2023-04-12 13:34:17 +0900

    adding correct answers for Lesson 4

---
+++ exercises/exercise_04_0_0_correct1.py
@@ -0,0 +1,54 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+# for loopをbreakで中断する練習問題   #
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+
+"""
+     ------   問題解説   -------
+
+以下に、zoo_animalsと、maybe_in_zooという二つのリストがあります。
+for loopの中で、maybe_in_zooの中から、zoo_animalsにも含まれる要素を
+検出し、ループをbreakにより中断させる。ループを中断すると、その時の
+animalという変数の値が返される。
+
+つまり、今回はget_first_zoo_animal()という関数が、どこかの
+プログラムで呼ばれると、その呼ばれたところに返り値を返す。
+関数の最後にreturn...というキーワードを入れると、
+値を「返り値」として返す働きがあります。returnについては、ここで
+特に考える必要はありません。後ほど関数のレッスンで詳しく
+勉強します。
+"""
+
+zoo_animals = [
+    "monkey", "turtle", "giraffe",
+    "tiger", "peacock", "polar bear", "panda",
+    "reindeer"
+]
+
+maybe_in_zoo = [
+    "turkey", "snake", "elephant", "cow", "horse",
+    "goat", "eagle", "panda", "rabbit", "rhino"
+]
+
+
+def get_first_zoo_animal():
+    for animal in maybe_in_zoo:
+        if animal in zoo_animals: # 正解例
+            break # 正解例
+    return animal
+
+print(get_first_zoo_animal())
+
+# ---
+
+def test_get_first_zoo_animal():
+    found = get_first_zoo_animal()
+    expected = "panda"
+    if found == expected:
+        print(f"正解です。出力：{found}")
+    else:
+        print(f"""
+残念でした。もう一度チャレンジしましょう。
+今回の要求出力は「{expected}」でしたが、「{found}」が出力されました。
+                """)
+
+test_get_first_zoo_animal()
---
+++ exercises/exercise_04_1_0_correct1.py
@@ -0,0 +1,47 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+# for loopでcontinueを利用する練習問題 #
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+
+"""
+     ------   問題解説   -------
+
+以下に、uncommon_wordsというリストがある。その中から"n"を含まない
+要素のみのリストを作ることが目標です。回答に必ずcontinueを
+使ってください。
+"""
+
+uncommon_words = [
+    'obfuscate', 'quixotic', 'surreptitious',
+    'sesquipedalian', 'lackadaisical', 'insouciant',
+    'effervescent', 'perspicacious', 'ebullient',
+    'recalcitrant', 'jejune', 'pulchritudinous',
+    'coruscate', 'defenestration', 'susurrus', 'lagniappe',
+    'discombobulate', 'lilliputian', 'macrosmatic', 'nidificate']
+
+
+def get_words_without_letter_n():
+    without_n = []
+    for word in uncommon_words: # 正解例
+        if 'n' not in word: # 正解例
+            without_n.append(word)
+    return without_n
+
+print(get_words_without_letter_n())
+
+
+
+# ---
+
+def test():
+    expected = [w for w in uncommon_words if 'n' not in w]
+    found = get_words_without_letter_n()
+
+    if set(found) == set(expected):
+        print(f"正解です。出力：{found}")
+    else:
+        print(f"""
+残念でした。もう一度チャレンジしましょう。
+今回の要求出力は「{expected}」でしたが、「{found}」が出力されました。
+                """)
+
+test()
